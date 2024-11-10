c6c8e82bfcbe82b6f5755890b94e08e1ce5a3129
mukhtar4532 <mukhtarhussain4532@gmail.com>
2024-11-10 19:15:48 +0530

    Project Completed

---
+++ backend/db/connectDB.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log("mongo_uri ", process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${connect.connection.host}`);
+  } catch (error) {
+    console.log("Error connection to MongoDB ", error.message);
+    process.exit(1); // 1 is for failure, 0 status code is success
+  }
+};
